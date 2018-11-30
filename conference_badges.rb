5d8b67bc35942c6fcebb09b2623cb8e9c14d13a2
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-11-30 01:07:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}." 
+end 
+  
+def batch_badge_creator(badges)
+  badges.collect do |attendees|
+    badge_maker(attendees) 
+  end
+end
+
+def assign_rooms(assignment)
+  assignment.each_with_index do |element, index|
+  "Hello, #{element}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer
+  
+end
